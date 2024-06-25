0d72f7d20178056d4e7f405b5a48ca59515901db
redaAssemghor <assemghor.reda@gmail.com>
2024-06-25 20:32:57 +0100

    eslint error

--- app/components/ui/wavy-background.tsx
+++ app/components/ui/wavy-background.tsx
@@ -97,6 +97,7 @@ export const WavyBackground = ({
     return () => {
       cancelAnimationFrame(animationId);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const [isSafari, setIsSafari] = useState(false);
