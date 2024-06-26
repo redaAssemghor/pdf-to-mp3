46ee27ec033842ad97af1946d4d6544d5d625c7a
redaAssemghor <assemghor.reda@gmail.com>
2024-06-26 19:13:35 +0100

    login logic

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const config = {
   providers: [
     {
-      domain: "https://equal-hare-92.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
